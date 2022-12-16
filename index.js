bfd60928a9ed93f58f3f4ca3e31d00b968a4dd7d
Nermin Softic <softicnermin@gmail.com>
2022-12-16 11:19:54 +0100

    First work

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+// const cors = require('cors');
+// const _ = require('lodash');
+// const { v4: uuid } = require('uuid');
+
+const app = express();
+app.use(express.json());
+const synonymsTable = new Map();
+
+app.get('/synonym/:word', (req, res) => {
+  const { word } = req.params;
+
+  console.log(word);
+
+  const synonym = synonymsTable.get(word);
+  if (synonym) {
+    synonym.forEach((el) => {
+      console.log(`The synonym for word ${word} is ${el}`);
+    });
+
+    res.json({
+      synonyms: Array.from(synonym),
+    });
+  } else {
+    res.json({
+      synonym: 'No synonym',
+    });
+  }
+});
+
+app.post('/synonym', (req, res) => {
+  const { word, newSynonym } = req.body;
+
+  // Is there already a synonym for this word
+  const existingWordSynonyms = synonymsTable.get(word);
+
+  if (existingWordSynonyms && existingWordSynonyms.has(newSynonym)) {
+    console.log('Synonym already saved!!!');
+    res.sendStatus(201);
+    return;
+  }
+  // If it's an existing word add the new
+  // synonym if not already present
+  if (existingWordSynonyms) {
+    console.log('-------  START ------');
+    console.log('Existing synonyms for this word:');
+    console.log(existingWordSynonyms);
+    // 1. Add the entry for the newSynony word
+    // It consists of the existing synonyms for the lookup word and that word itself
+    const newEntrySynyms = new Set(existingWordSynonyms);
+    newEntrySynyms.add(word);
+    synonymsTable.set(newSynonym, newEntrySynyms);
+    console.log(newEntrySynyms);
+
+    console.log('-----2-----');
+
+    // Add this synonym to the other synonyms due to transitive rule
+    // If "B" is a synonym to "A" and "C" a synonym to "B", then "C" should automatically,
+    // by transitive rule, also be the synonym for "A".
+    existingWordSynonyms.forEach((el) => {
+      const otherSyonyms = synonymsTable.get(el);
+      otherSyonyms.add(newSynonym);
+      console.log(el);
+    });
+
+    // add it to this word as well
+    existingWordSynonyms.add(newSynonym);
+
+    console.log(existingWordSynonyms);
+    console.log('-------  END ------');
+  } else {
+    // First entry
+    synonymsTable.set(word, new Set([newSynonym]));
+    synonymsTable.set(newSynonym, new Set([word]));
+  }
+
+  console.log(synonymsTable);
+  res.sendStatus(201);
+});
+
+app.listen(3000, () => console.log('listening'));
