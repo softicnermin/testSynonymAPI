2ed8109895ed10eb96443fe02d35e3f74e379327
Nermin Softic <softicnermin@gmail.com>
2022-12-17 22:22:47 +0100

    Add messages

--- controllers/synonymController.js
+++ controllers/synonymController.js
@@ -7,26 +7,18 @@ const postSynonym = ((req, res) => {
   const existingWordSynonyms = synonymsData.get(word);
 
   if (existingWordSynonyms && existingWordSynonyms.has(newSynonym)) {
-    console.log('Synonym already saved!!!');
-    res.sendStatus(200).json({
-      existingSynonyms: Array.from(existingWordSynonyms),
+    res.json({
+      message: 'Duplicate entry!',
     });
-    return;
-  }
-  // If it's an existing word add the new
-  // synonym if not already present
-  if (existingWordSynonyms) {
-    console.log('-------  START ------');
-    console.log('Existing synonyms for this word:');
-    console.log(existingWordSynonyms);
+  } else if (existingWordSynonyms) {
+    // If it's an existing word add the new
+    // synonym if not already present
+
     // 1. Add the entry for the newSynony word
     // It consists of the existing synonyms for the lookup word and that word itself
     const newEntrySynyms = new Set(existingWordSynonyms);
     newEntrySynyms.add(word);
     synonymsData.set(newSynonym, newEntrySynyms);
-    console.log(newEntrySynyms);
-
-    console.log('-----2-----');
 
     // Add this synonym to the other synonyms due to transitive rule
     // If "B" is a synonym to "A" and "C" a synonym to "B", then "C" should automatically,
@@ -34,22 +26,22 @@ const postSynonym = ((req, res) => {
     existingWordSynonyms.forEach((el) => {
       const otherSyonyms = synonymsData.get(el);
       otherSyonyms.add(newSynonym);
-      console.log(el);
     });
 
     // add it to this word as well
     existingWordSynonyms.add(newSynonym);
 
-    console.log(existingWordSynonyms);
-    console.log('-------  END ------');
+    res.json({
+      message: 'Success!',
+    });
   } else {
     // First entry
     synonymsData.set(word, new Set([newSynonym]));
     synonymsData.set(newSynonym, new Set([word]));
+    res.json({
+      message: 'Success!',
+    });
   }
-
-  console.log(synonymsData);
-  res.sendStatus(201);
 });
 
 const getSynonym = ((req, res) => {
