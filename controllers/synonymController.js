e70b86f77fb9e0062af04a7a8dfcd611a77b65db
Nermin Softic <softicnermin@gmail.com>
2022-12-24 00:54:19 +0100

    - Use uuid for SynonymData ids - Add new logic for saving synonyms

--- controllers/synonymController.js
+++ controllers/synonymController.js
@@ -1,8 +1,11 @@
+const { v4: uuidv4 } = require('uuid');
+
 const wordsDictionary = new Map();
 const synonymsData = new Map();
 
 const MAX_WORD_LENGTH = 35;
 const isWordValid = (word) => word && /^[a-zA-Z ]+$/.test(word) && word.length < MAX_WORD_LENGTH;
+const sanitizeWord = (word) => word.trim().toLowerCase();
 /*
   Storage
   wordsDictionary is a Map where the word is the key and
@@ -25,11 +28,13 @@ const isWordValid = (word) => word && /^[a-zA-Z ]+$/.test(word) && word.length <
  */
 
 const postSynonym = ((req, res) => {
-  const { word, newSynonym } = req.body;
+  let { firstWord, secondWord } = req.body;
+  firstWord = sanitizeWord(firstWord);
+  secondWord = sanitizeWord(secondWord);
   // If the words are not valid return bad request
-  if (!isWordValid(word)
-      || !isWordValid(newSynonym)
-      || word === newSynonym
+  if (!isWordValid(firstWord)
+      || !isWordValid(secondWord)
+      || firstWord === secondWord
   ) {
     res.status(400).json({
       message: 'Not valid entry!',
@@ -38,33 +43,73 @@ const postSynonym = ((req, res) => {
   }
 
   // Is there already a synonym for this word
-  const key = wordsDictionary.get(word);
-  const existingWordSynonyms = synonymsData.get(key);
+  const firstWordKey = wordsDictionary.get(firstWord);
+  const firstWordSynonyms = synonymsData.get(firstWordKey);
 
-  if (existingWordSynonyms && existingWordSynonyms.has(newSynonym)) {
+  // CASE I - a duplicate entry  ( w1-w1 )
+  if (firstWordSynonyms && firstWordSynonyms.has(secondWord)) {
     res.json({
       message: 'Duplicate entry!',
     });
-  } else if (existingWordSynonyms) {
+    return;
+  }
+
+  const secondWordKey = wordsDictionary.get(secondWord);
+  const secondWordSynonyms = synonymsData.get(secondWordKey);
+
+  // CASE II - Both words already exist but not as synonyms
+  // Now they are declared synonyms, so we need to merge the synonym list
+  // Example for the list in the description would be to
+  // declare any word from the group 1 a synonym with any of the words from group 2
+  if (firstWordKey && secondWordKey && firstWordKey !== secondWordKey) {
+    const mergedKey = synonymsData.size + 1;
+    // 1.New merged list
+    synonymsData.set(mergedKey, new Set([...firstWordSynonyms, ...secondWordSynonyms]));
+    // 2. Reassign synonym list keys
+    [...firstWordSynonyms, ...secondWordSynonyms].forEach((word) => {
+      wordsDictionary.set(word, mergedKey);
+    });
+    // 3. Delete old entries
+    synonymsData.delete(firstWordKey);
+    synonymsData.delete(secondWordKey);
+    res.json({
+      message: 'Success!',
+    });
+    return;
+  }
+  // CASE III - Only first word exists
+  if (firstWordKey && !secondWordKey) {
     // If it's an existing word add the new
     // synonym if not already present
-    existingWordSynonyms.add(newSynonym);
-    wordsDictionary.set(newSynonym, key);
+    firstWordSynonyms.add(secondWord);
+    wordsDictionary.set(secondWord, firstWordKey);
 
     res.json({
       message: 'Success!',
     });
-  } else {
-    // New entry
-    const newKey = synonymsData.size + 1;
-    synonymsData.set(newKey, new Set([word, newSynonym]));
-    // Add both wordsDictionary with the link to the synonym set
-    wordsDictionary.set(word, newKey);
-    wordsDictionary.set(newSynonym, newKey);
+    return;
+  }
+  // CASE IV - Only second word exists
+  if (secondWordKey && !firstWordKey) {
+    // If it's an existing word add the new
+    // synonym if not already present
+    secondWordSynonyms.add(firstWord);
+    wordsDictionary.set(firstWord, secondWordKey);
+
     res.json({
       message: 'Success!',
     });
+    return;
   }
+  // CASE V - First time for new words
+  const newKey = uuidv4();
+  synonymsData.set(newKey, new Set([firstWord, secondWord]));
+  // Add both wordsDictionary with the link to the synonym set
+  wordsDictionary.set(firstWord, newKey);
+  wordsDictionary.set(secondWord, newKey);
+  res.json({
+    message: 'Success!',
+  });
 });
 
 /*
